60d3539061a232bc503f5c42a2778d86c80bc476
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-11-13 16:58:06 -0500

    update

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,3 +1,4 @@
+import mbta_helper
 import urllib.request
 import json
 from pprint import pprint
@@ -7,8 +8,8 @@ MAPQUEST_BASE_URL = "http://open.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = L7NHzNvuOR9Gm0h12LJF6BBZ3855ha8D
-MBTA_API_KEY = 82629ccee2ca4a40b1410cb8711a184b
+MAPQUEST_API_KEY = 'L7NHzNvuOR9Gm0h12LJF6BBZ3855ha8D'
+MBTA_API_KEY = '6527b5fff8394156b8877887d9db2a65'
 
 
 
@@ -24,6 +25,26 @@ def get_json(url):
     response_text = f.read().decode('utf-8')
     response_data = json.loads(response_text)
     pprint(response_data)
+
+
+def get_lat_long(place_name):
+    """
+    Given a place name or address, return a (latitude, longitude) tuple
+    with the coordinates of the given place.
+    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
+    for Mapquest Geocoding  API URL formatting requirements.
+    """
+    pass
+
+
+def get_nearest_station(latitude, longitude):
+    """
+    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
+    tuple for the nearest MBTA station to the given coordinates.
+    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
+    formatting requirements for the 'GET /stops' API.
+    """
+    pass
 
 
 def find_stop_near(place_name):
