bbae5662658da840f77e411318e803bbb774995e
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-11-13 18:13:19 -0500

    update

--- mbta_helper.py
+++ mbta_helper.py
@@ -36,9 +36,7 @@ def get_lat_long(place_name):
     for Mapquest Geocoding  API URL formatting requirements.
     """
     url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name}'
-    f = urllib.request.urlopen(url)
-    response_text = f.read().decode('utf-8')
-    response_data = json.loads(response_text)
+    response_data = get_json(url)
     response_data1 = response_data["results"][0]["locations"][0]['latLng']
     print(response_data1)
     latlong_s = set(['lat','long'])
