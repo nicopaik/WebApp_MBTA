b2e101b75643708e913326000f2ff58d7eab1df6
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-11-13 16:35:11 -0500

    update

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,3 +1,7 @@
+import urllib.request
+import json
+from pprint import pprint
+
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPQUEST_BASE_URL = "http://open.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
@@ -12,30 +16,14 @@ MBTA_API_KEY = 82629ccee2ca4a40b1410cb8711a184b
 
 def get_json(url):
     """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
-    """
-    pass
-
-
-def get_lat_long(place_name):
-    """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding  API URL formatting requirements.
-    """
-    pass
-
-
-def get_nearest_station(latitude, longitude):
-    """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
+    Given a properly formatted URL for?agger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={L7NHzNvuOR9Gm0h12LJF6BBZ3855ha8D}&location=Babson%20College'
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    pprint(response_data)
 
 
 def find_stop_near(place_name):
