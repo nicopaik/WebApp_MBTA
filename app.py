d423778d9913d460e0f6bb113b7d6e424cfcc7ec
dchu07 <37084052+dchu07@users.noreply.github.com>
2019-11-13 21:26:23 -0500

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Amazing app to help you find nearest MBTA station
 """
 
-from flask import Flask
+from flask import Flask, render_template
 from mbta_helper import *
 
 
@@ -11,4 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index Page"
+    """
+    display index as homepage
+    """
+    return render_template('index.html')
